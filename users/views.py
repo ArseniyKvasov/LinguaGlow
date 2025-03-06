70f0f593ad1c21547eb1cba30daf94dfa952031c
ArseniyKvasov <94193304+ArseniyKvasov@users.noreply.github.com>
2025-03-06 17:56:18 +0300

    uploaded version 2

--- users/views.py
+++ users/views.py
@@ -11,11 +11,17 @@ def register_view(request):
             user = form.save(commit=False)
             user.set_password(form.cleaned_data['password1'])
             user.save()
-            login(request, user)  # Автоматически входим после регистрации
-            return redirect('home')  # Перенаправляем на главную страницу
+            login(request, user)
+            # Получаем параметр `next` из запроса
+            next_url = request.POST.get('next', None)
+            if next_url:
+                return redirect(next_url)  # Перенаправляем на `next`
+            else:
+                return redirect('home')  # Если `next` нет, перенаправляем на главную
     else:
+        next_url = request.GET.get('next', None)
         form = RegistrationForm()
-    return render(request, 'users/register.html', {'form': form})
+    return render(request, 'users/register.html', {'form': form, 'next': next_url})
 
 
 def login_view(request):
@@ -27,7 +33,14 @@ def login_view(request):
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                return redirect('home')  # Перенаправляем на главную страницу
+                # Получаем параметр `next` из запроса
+                next_url = request.POST.get('next', None)
+                if next_url:
+                    return redirect(next_url)  # Перенаправляем на `next`
+                else:
+                    return redirect('home')  # Если `next` нет, перенаправляем на главную
     else:
+        # Передаем параметр `next` в форму, если он есть в запросе
+        next_url = request.GET.get('next', None)
         form = LoginForm()
-    return render(request, 'users/login.html', {'form': form})
+    return render(request, 'users/login.html', {'form': form, 'next': next_url})
